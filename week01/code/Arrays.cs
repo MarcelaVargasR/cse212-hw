32a75ecf71b87530306e610c8c8410f667a15c0a
Marcela <lmvt.07@gmail.com>
2026-01-11 16:23:44 -0600

    Implement MultiplesOf function and add solution file

--- week01/code/Arrays.cs
+++ week01/code/Arrays.cs
@@ -1,33 +1,40 @@
 public static class Arrays
 {
-    /// <summary>
-    /// This function will produce an array of size 'length' starting with 'number' followed by multiples of 'number'.  For 
-    /// example, MultiplesOf(7, 5) will result in: {7, 14, 21, 28, 35}.  Assume that length is a positive
-    /// integer greater than 0.
-    /// </summary>
-    /// <returns>array of doubles that are the multiples of the supplied number</returns>
     public static double[] MultiplesOf(double number, int length)
     {
-        // TODO Problem 1 Start
-        // Remember: Using comments in your program, write down your process for solving this problem
-        // step by step before you write the code. The plan should be clear enough that it could
-        // be implemented by another person.
+        // create an array with the specified size
+        double[] result = new double[length];
 
-        return []; // replace this return statement with your own
+        // use an for to iterate through the array
+        for (int i = 0; i < length; i++)
+        {
+            // In each pocition store a multiple of the number
+            result[i] = number * (i + 1);
+        }
+
+        // return the array 
+        return result;
     }
 
-    /// <summary>
-    /// Rotate the 'data' to the right by the 'amount'.  For example, if the data is 
-    /// List<int>{1, 2, 3, 4, 5, 6, 7, 8, 9} and an amount is 3 then the list after the function runs should be 
-    /// List<int>{7, 8, 9, 1, 2, 3, 4, 5, 6}.  The value of amount will be in the range of 1 to data.Count, inclusive.
-    ///
-    /// Because a list is dynamic, this function will modify the existing data list rather than returning a new list.
-    /// </summary>
     public static void RotateListRight(List<int> data, int amount)
     {
-        // TODO Problem 2 Start
-        // Remember: Using comments in your program, write down your process for solving this problem
-        // step by step before you write the code. The plan should be clear enough that it could
-        // be implemented by another person.
+        // determine how many elements will be moved from the end
+        int count = data.Count;
+
+        // get the last element for the list
+        List<int> rightPart = data.GetRange(count - amount, amount);
+
+        // get the remaining element at the beginning for the list
+        List<int> leftPart = data.GetRange(0, count - amount);
+
+        // rebuild in the new order
+        data.Clear();
+
+        // add the right part first
+        data.AddRange(rightPart);
+
+        // add the remaining element after
+        data.AddRange(leftPart);
+
     }
 }
